7f603164df948b0a65ff3a1fedcd468778bb250d
sinqx <sinqxpls@gmail.com>
2023-09-28 17:20:11 +0600

    minor update

--- webApiReact/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ webApiReact/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -119,8 +119,6 @@ namespace webApiReact.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                user.K_PRED = Input.K_PRED;
-
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
