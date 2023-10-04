3e364eca29285d00fa1952e4f66cde94a8a19654
sinqx <sinqxpls@gmail.com>
2023-10-04 15:42:15 +0600

    test report-information page

--- webApiReact/ClientApp/src/components/UserReportInfo.js
+++ webApiReact/ClientApp/src/components/UserReportInfo.js
@@ -1,66 +1,151 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const UserReportInfo = () => {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const god = searchParams.get("god");
-  const kpred = searchParams.get("kpred");
-  const kvartal = searchParams.get("kvartal");
+  const { god, kvartal, k_PRED } = useParams();
+  const [report, setReport] = useState({});
+  const [editedReport, setEditedReport] = useState({});
 
-  const [reports, setReports] = useState([]);
-  console.log(god,kpred,kvartal);
   useEffect(() => {
     axios
       .get(
-        `https://localhost:7100/api/UserReport/getInfo?god=${god}&kpred=${kpred}&kvartal=${kvartal}`
+        `https://localhost:7100/api/UserReport/getInfo?god=${god}&kpred=${k_PRED}&kvartal=${kvartal}`
       )
       .then(function (response) {
-        // handle success
-        setReports(response.data);
-        console.log(response.data);
+        setReport(response.data);
+        setEditedReport(response.data);
       })
       .catch(function (error) {
-        // handle error
         console.log(error);
       });
-  }, [god, kpred, kvartal]);
+  }, [god, k_PRED, kvartal]);
+
+  const handleReportChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    const newValue = type === "checkbox" ? checked : value;
+
+    setEditedReport((prevReport) => ({
+      ...prevReport,
+      [name]: newValue,
+    }));
+
+    handleСhangeReport(name, newValue); // Вызов функции handleSaveReport при изменении поля
+  };
+
+  const handleСhangeReport = (propertyName, propertyValue) => {
+    const updatedReport = {
+      ...editedReport,
+      [propertyName]: propertyValue,
+    };
+
+    console.log(updatedReport);
+    axios
+      .patch("https://localhost:7100/api/UserReport/change", null, {
+        params: {
+          god: report.god,
+          k_pred: report.k_PRED,
+          kvartal: report.kvartal,
+        },
+        data: {
+          parameters: {
+            [propertyName]: propertyValue,
+          },
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then(function (response) {
+        console.log("Report saved successfully");
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log(error.response.data);
+      });
+  };
+
+  const handleSaveReport = () => {
+    console.log(editedReport);
+    axios
+      .put("https://localhost:7100/api/UserReport/replace", editedReport, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then(function (response) {
+        console.log("Report saved successfully");
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <div>
       <h1>Национальный Статистический Комитет</h1>
       <div className="container">
         <div className="row">
-          {reports.length > 0 ? (
-            reports.map((report) => (
-              <div
-                className="col-md-4"
-                key={`${report.GOD}_${report.kvartal}_${report.k_pred}`}
-              >
-                <div className="card mb-3">
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      Дата: {report.god}, квартал #{report.kvartal}
-                    </h5>
-                    <p className="card-text">
-                      Код предприятия: {report.k_PRED}
-                    </p>
-                    <a
-                      href={`/Reports/Details/${report.id}`}
-                      className="btn btn-primary"
-                    >
-                      Подробнее
-                    </a>
-                  </div>
-                </div>
+          <div className="col-md-4">
+            <div className="card mb-3">
+              <div className="card-body">
+                <h5 className="card-title">
+                  Дата: {report.god}, квартал #{report.kvartal}
+                </h5>
+                <p className="card-text">Код предприятия: {report.k_PRED}</p>
               </div>
-            ))
-          ) : (
-            <p>Данные отчетов загружаются...</p>
-          )}
+            </div>
+          </div>
         </div>
       </div>
+
+      {/* Отобразить поля отчета для редактирования */}
+      <h2>Редактирование отчета</h2>
+      <div>
+        <label>INDGR:</label>
+        <input
+          type="checkbox"
+          name="indgr"
+          checked={editedReport.indgr ?? report.indgr}
+          onChange={handleReportChange}
+        />
+      </div>
+      <div>
+        <label>date_UPDATE:</label>
+        <input
+          type="date"
+          name="date_UPDATE"
+          value={
+            (editedReport.date_UPDATE || report.date_UPDATE)?.split("T")[0]
+          }
+          onChange={handleReportChange}
+        />
+      </div>
+      <div>
+        <label>F_I_O:</label>
+        <input
+          type="text"
+          name="f_I_O"
+          value={editedReport.f_I_O ?? report.f_I_O}
+          onChange={handleReportChange}
+        />
+      </div>
+      <div>
+        <label>K_NPO:</label>
+        <input
+          type="text"
+          name="k_NPO"
+          value={editedReport.k_NPO ?? report.k_NPO}
+          onChange={handleReportChange}
+        />
+      </div>
+      {/* Добавьте остальные поля для редактирования */}
+
+      {/* Кнопка сохранения отчета */}
+      <button onClick={handleSaveReport}>Сохранить</button>
     </div>
   );
 };
