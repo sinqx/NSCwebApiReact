a65061ba4200de4ef0dc40fc6393f0742fe037d5
sinqx <sinqxpls@gmail.com>
2023-10-05 17:15:53 +0600

    testing

--- webApiReact/ClientApp/src/components/UserReportInfo.js
+++ webApiReact/ClientApp/src/components/UserReportInfo.js
@@ -87,7 +87,7 @@ const UserReportInfo = () => {
   return (
     <div>
       <h1>Национальный Статистический Комитет</h1>
-      <div className="container">
+      <div className="content_container">
         <div className="row">
           <div className="col-md-4">
             <div className="card mb-3">
@@ -100,52 +100,56 @@ const UserReportInfo = () => {
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Отобразить поля отчета для редактирования */}
-      <h2>Редактирование отчета</h2>
-      <div>
-        <label>INDGR:</label>
-        <input
-          type="checkbox"
-          name="indgr"
-          checked={editedReport.indgr ?? report.indgr}
-          onChange={handleReportChange}
-        />
-      </div>
-      <div>
-        <label>date_UPDATE:</label>
-        <input
-          type="date"
-          name="date_UPDATE"
-          value={
-            (editedReport.date_UPDATE || report.date_UPDATE)?.split("T")[0]
-          }
-          onChange={handleReportChange}
-        />
-      </div>
-      <div>
-        <label>F_I_O:</label>
-        <input
-          type="text"
-          name="f_I_O"
-          value={editedReport.f_I_O ?? report.f_I_O}
-          onChange={handleReportChange}
-        />
-      </div>
-      <div>
-        <label>K_NPO:</label>
-        <input
-          type="text"
-          name="k_NPO"
-          value={editedReport.k_NPO ?? report.k_NPO}
-          onChange={handleReportChange}
-        />
-      </div>
-      {/* Добавьте остальные поля для редактирования */}
+        {/* Отобразить поля отчета для редактирования */}
+        <h2>Редактирование отчета</h2>
+        <div>
+          <label>INDGR:</label>
+          <input
+            type="checkbox"
+            name="indgr"
+            checked={editedReport.indgr ?? report.indgr}
+            onChange={handleReportChange}
+          />
+        </div>
+        <div>
+          <label>date_UPDATE:</label>
+          <input
+            type="date"
+            name="date_UPDATE"
+            value={
+              (editedReport.date_UPDATE || report.date_UPDATE)?.split("T")[0]
+            }
+            onChange={handleReportChange}
+          />
+        </div>
+        <div>
+          <label>F_I_O:</label>
+          <input
+            type="text"
+            name="f_I_O"
+            value={editedReport.f_I_O ?? report.f_I_O}
+            onChange={handleReportChange}
+          />
+        </div>
+        
+        <div className="form-floating mb-3">
+          <input
+            type="text"
+            name="k_NPO"
+            value={editedReport.k_NPO ?? report.k_NPO}
+            onChange={handleReportChange}
+            className="form-control"
+            aria-required="true"
+            placeholder="Пусто"
+          />
+          <label>k_NPO</label>
+        </div>
+        {/* Добавьте остальные поля для редактирования */}
 
-      {/* Кнопка сохранения отчета */}
-      <button onClick={handleSaveReport}>Сохранить</button>
+        {/* Кнопка сохранения отчета */}
+        <button onClick={handleSaveReport}>Сохранить</button>
+      </div>
     </div>
   );
 };
