ea84219c2dda2b748c14c8524cc57060b18f5ccd
sinqx <sinqxpls@gmail.com>
2023-10-05 17:14:33 +0600

    testing user auth

--- webApiReact/ClientApp/src/components/Home.js
+++ webApiReact/ClientApp/src/components/Home.js
@@ -1,28 +1,76 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export class Home extends Component {
-    static displayName = Home.name;
+    
+  static displayName = Home.name;
 
-    render() {
-        return (
-            <div>
-                <h1>НацСтатКом</h1>
-                <p>Welcome to your new single-page application, built with:</p>
-                <ul>
-                    <meta charset="utf-8" />
-                    <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-                    <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-                    <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-                </ul>
-                <p>To help you get started, we have also set up:</p>
-                <ul>
-                    <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-                    <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-                    <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-                </ul>
-                <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-            </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      loading: true
+    };
+  }
 
-        );
+  componentDidMount() {
+    this.fetchUserInfo();
+  }
+
+  fetchUserInfo = async () => {
+    try {
+      const response = await axios.get('https://localhost:7100/api/User/info');
+      const user = response.data;
+      this.setState({ user, loading: false });
+    } catch (error) {
+      console.error(error);
+      this.setState({ loading: false });
     }
+  }
+
+  render() {
+    const { user, loading } = this.state;
+
+    return (
+      <div>
+        <h1>{user ? user.username : 'Not logged in 1'}</h1>
+        <p>{user ? `Welcome, ${user.username}` : 'Not logged in 2'}</p>
+        <ul>
+          <meta charset="utf-8" />
+          <li>
+            <a href="https://get.asp.net/">ASP.NET Core</a> and{' '}
+            <a href="https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx">C#</a> НацСтатКом
+          </li>
+          <li>
+            <a href="https://facebook.github.io/react/">React</a> НацСтатКом
+          </li>
+          <li>
+            <a href="http://getbootstrap.com/">Bootstrap</a> НацСтатКом
+          </li>
+        </ul>
+        <p>
+          To help you get started, we have also set up НацСтатКом:
+        </p>
+        <ul>
+          <li>
+            <strong>Client-side НацСтатКом</strong>. For example, click <em>НацСтатКом</em> then{' '}
+            <em>НацСтатКом</em> to НацСтатКом.
+          </li>
+          <li>
+            <strong>Development НацСтатКом </strong>. In НацСтатКом mode, the development НацСтатКом from{' '}
+            <code>create-НацСтатКом-app</code> runs in the background automatically, so your НацСтатКом-side
+            resources are dynamically built on demand and the НацСтатКом refreshes when you modify any
+            НацСтатКом.
+          </li>
+          <li>
+            <strong>Efficient production НацСтатКом</strong>. In production НацСтатКом, development-time
+            features are НацСтатКом, and your <code>НацСтатКом publish</code> configuration produces minified,
+            efficiently НацСтатКом JavaScript files.
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
+
+export default Home;
