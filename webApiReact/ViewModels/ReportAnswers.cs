686530f2741f7652a5c5c49d016e44204e0240bd
sinqx <sinqxpls@gmail.com>
2023-10-11 17:23:22 +0600

    new model for report answers

---
+++ webApiReact/ViewModels/ReportAnswers.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webApiReact.ViewModels
+{
+    public class ReportAnswers
+    {
+        public int? P1 { get; set; }
+        public int? P2 { get; set; }
+        public int? p3 { get; set; }
+        public int? p4 { get; set; }
+        public int? p5 { get; set; }
+        public int? p6 { get; set; }
+        public int? p7 { get; set; }
+        public int? p8 { get; set; }
+        public int? p9 { get; set; }
+        public int? p10 { get; set; }
+    }
+}
