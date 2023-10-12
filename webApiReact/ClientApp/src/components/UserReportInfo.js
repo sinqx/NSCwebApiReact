65419a956c748735e276e4d9b948d49b89853fcd
sinqx <sinqxpls@gmail.com>
2023-10-12 16:47:11 +0600

    report info page update

--- webApiReact/ClientApp/src/components/UserReportInfo.js
+++ webApiReact/ClientApp/src/components/UserReportInfo.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import "./UserReportInfo.css";
 
 const UserReportInfo = () => {
-  const { god, kvaratl, k_PRED } = useParams();
+  const [error, setError] = useState(null);
   const [report, setReport] = useState({});
-  const [editedReport, setEditedReport] = useState({});
+  const [answers, setAnswers] = useState({});
+  const { god, kvaratl, k_PRED } = useParams();
   const [searchGod, setSearchGod] = useState("");
-  const [searchKvaratl, setSearchKvaratl] = useState("");
   const [searchK_PRED, setSearchK_PRED] = useState("");
+  const [searchKvaratl, setSearchKvaratl] = useState("");
+  const [searchResult, setSearchResult] = useState(false);
 
   const handleSearch = () => {
     axios
@@ -17,11 +19,25 @@ const UserReportInfo = () => {
         `https://localhost:7100/api/UserReport/getInfo?god=${searchGod}&kpred=${searchK_PRED}&kvaratl=${searchKvaratl}`
       )
       .then(function (response) {
-        setReport(response.data);
-        setEditedReport(response.data);
+        console.log(response);
+        const fetchedReport = response.data;
+        const initialAnswers = {};
+
+        for (let i = 1; i <= 10; i++) {
+          const propName = `p${i}`;
+          initialAnswers[propName] = fetchedReport[propName.toLowerCase()];
+        }
+
+        setReport(fetchedReport);
+        setAnswers(initialAnswers);
+        setSearchResult(true); // Результат поиска найден
+        setError(null); // Сбросить ошибку
+     
       })
       .catch(function (error) {
         console.log(error);
+        setSearchResult(false); // Результат поиска не найден
+        setError(error.response.data.detail); // Установить значение ошибки
       });
   };
 
@@ -30,31 +46,35 @@ const UserReportInfo = () => {
   }, []);
 
   const handleResetReport = () => {
-    setEditedReport(report);
+    setAnswers(report);
   };
 
-  const handleUpdateReport = (updatedReport) => {
-    const { name, value, type, checked } = updatedReport.target;
+  const handleUpdateAnswers = (e) => {
+    const { name, value, type, checked } = e.target;
     const newValue = type === "checkbox" ? checked : value;
     // Проверка на ввод знака минус и символа "e"
-    if (
-      type === "number" &&
-      (updatedReport.key === "-" || updatedReport.key === "e")
-    ) {
-      updatedReport.preventDefault();
+    if (type === "number" && (e.key === "-" || e.key === "e")) {
+      e.preventDefault();
       return;
     }
     const sanitizedValue = value === "" ? null : newValue;
 
-    setEditedReport((prevReport) => ({
-      ...prevReport,
-      [name]: sanitizedValue,
-    }));
+    if (name.startsWith("p")) {
+      setAnswers((prevAnswers) => ({
+        ...prevAnswers,
+        [name]: sanitizedValue,
+      }));
+    }
   };
 
   const handleSaveReport = () => {
+    answers.GOD = report.god;
+    answers.k_PRED = report.k_PRED;
+    answers.Kvaratl = report.kvaratl;
+    console.log(answers);
+    console.log(report);
     axios
-      .put("https://localhost:7100/api/UserReport/replace", editedReport, {
+      .put(`https://localhost:7100/api/UserReport/change`, answers, {
         headers: {
           "Content-Type": "application/json",
         },
@@ -62,6 +82,7 @@ const UserReportInfo = () => {
       .then(function (response) {
         console.log("Report saved successfully");
         console.log(response);
+        console.log(answers);
       })
       .catch(function (error) {
         console.log(error);
@@ -109,200 +130,206 @@ const UserReportInfo = () => {
           </div>
         </div>
 
+        {/* Отобразить ошибку, если есть */}
+        {error && (
+          <div>
+            <p>{error}</p>{" "}
+          </div>
+        )}
+
         {/* Отобразить поля отчета для редактирования */}
-        <h2>Редактирование отчета</h2>
-        <table className="iksweb mb-4">
-          <tbody>
-            <tr>
-              <td>Наименование показателя </td>
-              <td>Код строки </td>
-              <td>Единицы измерения</td>
-              <td>
-                {report.tiP2 === 1
-                  ? "Базар/рынок"
-                  : report.tiP2 === 2
-                  ? "Торговый центр"
-                  : "Место"}{" "}
-              </td>
-            </tr>
-            <tr>
-              <td>A</td>
-              <td>Б</td>
-              <td>В</td>
-              <td>{report.tiP2}</td>
-            </tr>
-            <tr>
-              <td>
-                Число торговых мест и торговых объектов (кроме магазинов),
-                отведенных под торговлю –всего{" "}
-              </td>
-              <td>01</td>
-              <td>единиц </td>
-              <td>
-                <input
-                  type="number"
-                  name="p1"
-                  value={editedReport.p1}
-                  onChange={handleUpdateReport}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>из них на которых фактически осуществлялась торговля </td>
-              <td>02</td>
-              <td>единиц </td>
-              <td>
-                <input
-                  type="number"
-                  name="p2"
-                  value={editedReport.p2}
-                  onChange={handleUpdateReport}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>в том числе: юридическими лицами</td>
-              <td>03</td>
-              <td>единиц </td>
-              <td>
-                <input
-                  type="number"
-                  name="p3"
-                  value={editedReport.p3}
-                  onChange={handleUpdateReport}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                хозяйствующими субъектами, имеющими статус физического лица
-              </td>
-              <td>04</td>
-              <td>единиц </td>
-              <td>
-                <input
-                  type="number"
-                  name="p4"
-                  value={editedReport.p4}
-                  onChange={handleUpdateReport}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>индивидуальными предпринимателями</td>
-              <td>05</td>
-              <td>единиц </td>
-              <td>
-                <input
-                  type="number"
-                  name="p5"
-                  value={editedReport.p5}
-                  onChange={handleUpdateReport}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>Количество магазинов </td>
-              <td>06</td>
-              <td>единиц </td>
-              <td>
-                <input
-                  type="number"
-                  name="p6"
-                  value={editedReport.p6}
-                  onChange={handleUpdateReport}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Торговая площадь, отведенная под торговые объекты индивидуальных
-                предпринимателей
-              </td>
-              <td>07</td>
-              <td>м²</td>
-              <td>
-                {report.tiP2 === 1 ? (
-                  "Х"
-                ) : (
-                  <input
-                    type="number"
-                    name="p7"
-                    value={editedReport.p7}
-                    onChange={handleUpdateReport}
-                    readOnly={report.tiP2 === 1}
-                  />
-                )}
-              </td>
-            </tr>
-            <tr>
-              <td>Средняя стоимость 1 места в день</td>
-              <td>08</td>
-              <td>сомов</td>
-              <td>
-                {report.tiP2 === 2 ? (
-                  "Х"
-                ) : (
-                  <input
-                    type="number"
-                    name="p8"
-                    value={editedReport.p8}
-                    onChange={handleUpdateReport}
-                    readOnly={report.tiP2 === 2}
-                  />
-                )}
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Общий сбор за пользование торговым местом на рынке (за квартал)
-              </td>
-              <td>09</td>
-              <td>тыс.сомов</td>
-              <td>
-                {report.tiP2 === 2 ? (
-                  "Х"
-                ) : (
-                  <input
-                    type="number"
-                    name="p9"
-                    value={editedReport.p9}
-                    onChange={handleUpdateReport}
-                    readOnly={report.tiP2 === 2}
-                  />
-                )}
-              </td>
-            </tr>
-            <tr>
-              <td>Количество дней работы за квартал</td>
-              <td>10</td>
-              <td>дней </td>
-              <td>
-                <input
-                  type="number"
-                  name="p10"
-                  value={editedReport.p10}
-                  onChange={handleUpdateReport}
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        {/* Кнопка сохранения отчета */}
-        <div className="buttons-container">
-          <button
-            className="btn btn-save"
-            onClick={handleSaveReport}
-          >
-            Сохранить
-          </button>
-          <button
-            className="btn btn-reset"
-            onClick={handleResetReport}
-          >
-            Сбросить
-          </button>
-        </div>
+        {searchResult && (
+          <div>
+            <h2>Редактирование отчета</h2>
+            <table className="iksweb mb-4">
+              <tbody>
+                <tr>
+                  <td>Наименование показателя </td>
+                  <td>Код строки </td>
+                  <td>Единицы измерения</td>
+                  <td>
+                    {report.tiP2 === 1
+                      ? "Базар/рынок"
+                      : report.tiP2 === 2
+                      ? "Торговый центр"
+                      : "Место"}{" "}
+                  </td>
+                </tr>
+                <tr>
+                  <td>A</td>
+                  <td>Б</td>
+                  <td>В</td>
+                  <td>{report.tiP2}</td>
+                </tr>
+                <tr>
+                  <td>
+                    Число торговых мест и торговых объектов (кроме магазинов),
+                    отведенных под торговлю –всего{" "}
+                  </td>
+                  <td>01</td>
+                  <td>единиц </td>
+                  <td>
+                    <input
+                      type="number"
+                      name="p1"
+                      value={answers.p1}
+                      onChange={handleUpdateAnswers}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>из них на которых фактически осуществлялась торговля </td>
+                  <td>02</td>
+                  <td>единиц </td>
+                  <td>
+                    <input
+                      type="number"
+                      name="p2"
+                      value={answers.p2}
+                      onChange={handleUpdateAnswers}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>в том числе: юридическими лицами</td>
+                  <td>03</td>
+                  <td>единиц </td>
+                  <td>
+                    <input
+                      type="number"
+                      name="p3"
+                      value={answers.p3}
+                      onChange={handleUpdateAnswers}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    хозяйствующими субъектами, имеющими статус физического лица
+                  </td>
+                  <td>04</td>
+                  <td>единиц </td>
+                  <td>
+                    <input
+                      type="number"
+                      name="p4"
+                      value={answers.p4}
+                      onChange={handleUpdateAnswers}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>индивидуальными предпринимателями</td>
+                  <td>05</td>
+                  <td>единиц </td>
+                  <td>
+                    <input
+                      type="number"
+                      name="p5"
+                      value={answers.p5}
+                      onChange={handleUpdateAnswers}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Количество магазинов </td>
+                  <td>06</td>
+                  <td>единиц </td>
+                  <td>
+                    <input
+                      type="number"
+                      name="p6"
+                      value={answers.p6}
+                      onChange={handleUpdateAnswers}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Торговая площадь, отведенная под торговые объекты
+                    индивидуальных предпринимателей
+                  </td>
+                  <td>07</td>
+                  <td>м²</td>
+                  <td>
+                    {report.tiP2 === 1 ? (
+                      "Х"
+                    ) : (
+                      <input
+                        type="number"
+                        name="p7"
+                        value={answers.p7}
+                        onChange={handleUpdateAnswers}
+                        readOnly={report.tiP2 === 1}
+                      />
+                    )}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Средняя стоимость 1 места в день</td>
+                  <td>08</td>
+                  <td>сомов</td>
+                  <td>
+                    {report.tiP2 === 2 ? (
+                      "Х"
+                    ) : (
+                      <input
+                        type="number"
+                        name="p8"
+                        value={answers.p8}
+                        onChange={handleUpdateAnswers}
+                        readOnly={report.tiP2 === 2}
+                      />
+                    )}
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Общий сбор за пользование торговым местом на рынке (за
+                    квартал)
+                  </td>
+                  <td>09</td>
+                  <td>тыс.сомов</td>
+                  <td>
+                    {report.tiP2 === 2 ? (
+                      "Х"
+                    ) : (
+                      <input
+                        type="number"
+                        name="p9"
+                        value={answers.p9}
+                        onChange={handleUpdateAnswers}
+                        readOnly={report.tiP2 === 2}
+                      />
+                    )}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Количество дней работы за квартал</td>
+                  <td>10</td>
+                  <td>дней </td>
+                  <td>
+                    <input
+                      type="number"
+                      name="p10"
+                      value={answers.p10}
+                      onChange={handleUpdateAnswers}
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            {/* Кнопка сохранения отчета */}
+            <div className="buttons-container">
+              <button className="btn btn-save" onClick={handleSaveReport}>
+                Сохранить
+              </button>
+              <button className="btn btn-reset" onClick={handleResetReport}>
+                Сбросить
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
